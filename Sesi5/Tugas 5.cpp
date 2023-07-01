30444b6ccf26eac96293c1369ac9f8b04b3c8078
FebriTPP <113004186+FebriTPP@users.noreply.github.com>
2023-07-01 08:51:22 +0700

    Update Tugas 5.cpp

--- Sesi5/Tugas 5.cpp
+++ Sesi5/Tugas 5.cpp
@@ -1,60 +1,84 @@
 #include <iostream>
+
 using namespace std;
 
+// Struktur Node
 struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
-Node* createNode(int value) {
+// Fungsi untuk membuat Node baru
+Node* createNode(int data) {
     Node* newNode = new Node();
-    if (newNode == NULL) {
-        cout << "Error creating node!" << endl;
-        exit(1);
+    if (!newNode) {
+        cout << "Error dalam membuat Node." << endl;
+        return NULL;
     }
-    newNode->data = value;
+    newNode->data = data;
     newNode->left = newNode->right = NULL;
     return newNode;
 }
 
-Node* insertNode(Node* root, int value) {
+// Fungsi untuk memasukkan bilangan ke dalam tree
+Node* insertNode(Node* root, int data) {
     if (root == NULL) {
-        root = createNode(value);
+        root = createNode(data);
         return root;
     }
-    if (value < root->data) {
-        root->left = insertNode(root->left, value);
-    }
-    else {
-        root->right = insertNode(root->right, value);
-    }
+    if (data < root->data)
+        root->left = insertNode(root->left, data);
+    else if (data > root->data)
+        root->right = insertNode(root->right, data);
     return root;
 }
 
+// Fungsi untuk mencetak Node secara pre-order
 void preOrderTraversal(Node* root) {
-    if (root == NULL) {
+    if (root == NULL)
         return;
-    }
     cout << root->data << " ";
     preOrderTraversal(root->left);
     preOrderTraversal(root->right);
 }
 
+// Fungsi untuk mencetak Node secara in-order
+void inOrderTraversal(Node* root) {
+    if (root == NULL)
+        return;
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+
+// Fungsi untuk mencetak Node secara post-order
+void postOrderTraversal(Node* root) {
+    if (root == NULL)
+        return;
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
 int main() {
     Node* root = NULL;
+    int numbers[] = {12, 16, 20, 24, 32};
 
-    // Menambahkan bilangan ke dalam tree
-    root = insertNode(root, 12);
-    root = insertNode(root, 16);
-    root = insertNode(root, 20);
-    root = insertNode(root, 24);
-    root = insertNode(root, 32);
+    // Memasukkan bilangan ke dalam tree
+    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
+        root = insertNode(root, numbers[i]);
 
-    // Menampilkan tree secara pre-order
-    cout << "Pre-order traversal: ";
+    cout << "Traversal Pre-order: ";
     preOrderTraversal(root);
     cout << endl;
 
+    cout << "Traversal In-order: ";
+    inOrderTraversal(root);
+    cout << endl;
+
+    cout << "Traversal Post-order: ";
+    postOrderTraversal(root);
+    cout << endl;
+
     return 0;
-}
